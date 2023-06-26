5a4344f455f4cc28de28824ba9dfce5c64f90532
Ojas-Sharma <ojassharma1607@gmail.com>
2023-06-26 03:17:42 +0000

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VisualFlow',
-    version='0.1.4',
+    version='0.1.5',
     author='Ojas Sharma',
     author_email='ojassharma1607@gmail.com',
     description='A Python library for object detection format conversion',
