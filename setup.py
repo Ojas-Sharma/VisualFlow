7de260035b76f9fb12740a39e0743dd78ec6265b
Ojas-Sharma <ojassharma1607@gmail.com>
2023-07-19 20:10:09 +0000

    minor changes to augmentations.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VisualFlow',
-    version='0.1.7',
+    version='0.1.8',
     author='Ojas Sharma',
     author_email='ojassharma1607@gmail.com',
     description='A Python library for object detection format conversion',
